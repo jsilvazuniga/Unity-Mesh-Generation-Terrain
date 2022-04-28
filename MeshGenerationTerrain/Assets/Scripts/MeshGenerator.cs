2eadcb3d86eefa405227b4032cde8ab6861e5b49
Jorge Ricardo Silva Zuniga <jsilvazuniga@gmail.com>
2022-04-28 17:15:05 +0200

    join and creation squares

--- MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
+++ MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
@@ -19,17 +19,18 @@ public class MeshGenerator : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
-        UpdateMesh();
+        StartCoroutine(CreateShape());
+      
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        UpdateMesh();
     }
 
-    void CreateShape()
+
+    IEnumerator CreateShape()
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
@@ -37,10 +38,38 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                vertices [i] = new Vector3(x, 0, z);
                 i++;
             }
         }
+
+        triangles = new int[ xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return new WaitForSeconds(.1f);
+            }
+            vert++;
+        }
+
+
+       
     }
 
     void UpdateMesh()
