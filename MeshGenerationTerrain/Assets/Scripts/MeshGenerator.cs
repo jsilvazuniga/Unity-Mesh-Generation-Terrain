7edce6e50c91f66ec72b012662a6dc7d4d89be32
Jorge Ricardo Silva Zuniga <jsilvazuniga@gmail.com>
2022-04-28 16:51:37 +0200

    Creation Vertex

--- MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
+++ MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
@@ -9,6 +9,9 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,18 +31,16 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0),
-            new Vector3(1,0,1),
-        };
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-        triangles = new int[]{
-            0, 1, 2,
-            1, 3,2
-         };
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
     }
 
     void UpdateMesh()
@@ -51,4 +52,15 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.RecalculateNormals();
     }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+
 }
