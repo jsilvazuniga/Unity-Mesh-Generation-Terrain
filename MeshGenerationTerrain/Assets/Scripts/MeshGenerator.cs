0756886e1ba68ee4d7ce14cf51ea443ae4dc63db
Jorge Ricardo Silva Zuniga <jsilvazuniga@gmail.com>
2022-04-28 16:31:54 +0200

    Square

--- MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
+++ MeshGenerationTerrain/Assets/Scripts/MeshGenerator.cs
@@ -33,10 +33,12 @@ public class MeshGenerator : MonoBehaviour
             new Vector3(0,0,0),
             new Vector3(0,0,1),
             new Vector3(1,0,0),
+            new Vector3(1,0,1),
         };
 
         triangles = new int[]{
-            0,1,2
+            0, 1, 2,
+            1, 3,2
          };
     }
 
